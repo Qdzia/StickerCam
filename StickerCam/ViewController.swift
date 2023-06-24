9430db5a1332413176ddae6a12de95480444b1c3
Łukasz Kudzia <qdzia.work@gmail.com>
2023-06-24 18:46:51 +0200

    Init project

---
+++ StickerCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickerCam
+//
+//  Created by Łukasz Kudzia on 24/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
