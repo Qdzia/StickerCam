a52a4aaf21909b800f5498e420bb80fc3799d0d8
Łukasz Kudzia <qdzia.work@gmail.com>
2023-06-25 13:36:12 +0200

    Add font placeholder

---
+++ StickerCam/Utils/Fonts.swift
@@ -0,0 +1,14 @@
+//
+//  Fonts.swift
+//  StickerCam
+//
+//  Created by Łukasz Kudzia on 25/06/2023.
+//
+
+import UIKit
+
+extension UIFont {
+    static let title = UIFont(name: "Arial", size: 32)
+    static let subtitle = UIFont(name: "Arial", size: 24)
+    static let body = UIFont(name: "Comic Sans MS", size: 16)
+}
